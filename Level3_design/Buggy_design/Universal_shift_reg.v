1debb0a330f9621f1a94480705ba6a1205e48811
RaviMugidi <109639328+RaviMugidi@users.noreply.github.com>
2022-07-31 00:08:13 +0530

    buggy_design

--- Level3_design/Buggy_design/Universal_shift_reg.v
+++ Level3_design/Buggy_design/Universal_shift_reg.v
@@ -1,5 +1,6 @@
-module Universal_Reg(clk,serial_in,sel,in,out);
+module Universal_Reg(clk,serial_in,sel,in,reset,out);
 	input clk;
+	input reset;
 	input serial_in;
 	input [3:0]in;
 	input [1:0]sel;
@@ -8,11 +9,16 @@ module Universal_Reg(clk,serial_in,sel,in,out);
 	
 	always@(posedge clk)
 	begin
-	  case(sel)
-	      2'b00: out <= out;
-      2'b01: out <= {serial_in,out[2:0]};
-			  2'b10: out <= {serial_in,out[3:1]};
-			  2'b11: out <= in;
-	  endcase
+		if(reset==1)
+			out<=0;
+	         else
+			 begin
+			 case(sel)
+	                    2'b00: out <= out;
+                            2'b01: out <= {serial_in,out[2:0]};
+			    2'b10: out <= {serial_in,out[3:1]};
+			    2'b11: out <= in;
+	                  endcase
+	                  end
 	end
 endmodule
